f63100053ceb3c205cdc40aa1d03d2862dc45732
“Tamilmani321” <Tamilmanisa12@gmail.com>
2023-11-03 11:39:03 +0530

    some modification in main method and i have added a method for printing

--- HelloExample/src/com/example/Hello.java
+++ HelloExample/src/com/example/Hello.java
@@ -3,9 +3,14 @@ package com.example;
 public class Hello {
 
 	public static void main(String[] args) {
+		
+		
+
+	}
+	
+	private void display() {
 		System.out.println("this is java...");
 		System.out.println("This is hellow");
-
 	}
 
 }
